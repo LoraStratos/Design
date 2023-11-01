b811b74702e60ab23b9a9b552847fc8edd3017ea
LoraStratos <LoraStratosSi@mail.ru>
2023-11-01 13:03:44 +0700

    создание директории

--- design/catalog/models.py
+++ design/catalog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Application(models.Model):
+    title = models.CharField(max_length=200)
+    summary = models.TextField(max_length=1000, help_text="Опишите свою заявку здесь")
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, help_text="Введите категорию")
+    def __str__(self):
+        return self.name
