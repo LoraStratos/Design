d107d6f8298b75c9a500d891c50ae351ef7ced54
LoraStratos <LoraStratosSi@mail.ru>
2023-11-02 13:55:50 +0700

    настройки логина

--- design/catalog/views.py
+++ design/catalog/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'registration/login.html')
+
+def logout(request):
+    return render(request, 'registration/logout.html')
+
+def registration(request):
+    return render(request, 'registration/registration.html')
