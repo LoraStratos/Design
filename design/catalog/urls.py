315aa4ef8bb779a6271c38e73df456654a834d50
LoraStratos <LoraStratosSi@mail.ru>
2023-11-10 16:52:47 +0700

    начало 2.2

--- design/catalog/urls.py
+++ design/catalog/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
 from django.contrib.auth.views import LoginView, LogoutView
 from .views import *
 
